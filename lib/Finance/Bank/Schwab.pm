909ff90403b56537ef6d0e778c46297bf787dda7
Mark Grimes <mgrimes@cpan.org>
2013-12-10 10:31:54 -0500

    Refactors get position routines
    - Splits into subroutines (still a bit bulky)
    - Returns a list of position objects
    - Runs Perl::Tidy over everything
    - Add the get_position flag; no change in behaviour unless flag is set

--- lib/Finance/Bank/Schwab.pm
+++ lib/Finance/Bank/Schwab.pm
@@ -1,6 +1,6 @@
 package Finance::Bank::Schwab;
 
-# ABSTRCT: Check your account balances at Charles Schwab.
+# ABSTRACT: Check your account balances at Charles Schwab
 
 use strict;
 use warnings;
@@ -25,52 +25,8 @@ our $ua = WWW::Mechanize->new(
 
 sub check_balance {
     my ( $class, %opts ) = @_;
-    my $content;
 
-    if ( $opts{content} ) {
-
-        # If we give it a file, use the file rather than downloading
-        open my $fh, "<", $opts{content} or confess;
-        $content = do { local $/ = undef; <$fh> };
-        close $fh;
-
-    } else {
-
-        croak "Must provide a password" unless exists $opts{password};
-        croak "Must provide a username" unless exists $opts{username};
-
-        my $self = bless {%opts}, $class;
-
-        # Get the login page
-        $ua->get(
-            'https://client.schwab.com/Login/SignOn/CustomerCenterLogin.aspx')
-          or croak "couldn't load inital page";
-
-        # Find the login form, change the action url, then set the username/
-        # password and submit
-        my $login_form = $ua->form_name('aspnetForm')
-          or croak "Couldn't find the login form";
-        $login_form->action(
-            'https://client.schwab.com/Login/SignOn/signon.ashx')
-          or croak "Couldn't update the action url on login form";
-        my $username_field =
-          'ctl00$WebPartManager1$CenterLogin$LoginUserControlId$txtLoginID';
-        $login_form->value( $username_field => $opts{username} );
-        $login_form->value( 'txtPassword'   => $opts{password} );
-        $ua->submit() or croak "couldn't sign on to account";
-
-        $content = $ua->content;
-    }
-
-    if ( $opts{log} ) {
-
-        # Dump to the filename passed in log
-        open( my $fh, ">", $opts{log} ) or confess;
-        print $fh $content;
-        close $fh;
-    }
-
-    my @accounts;
+    my $content = retrieve_summary_page(%opts);
 
     my $te = HTML::TableExtract->new(
         headers => [
@@ -81,15 +37,14 @@ sub check_balance {
         ## decode    => 0,
     );
 
-    {
-
-        # HTML::TableExtract warns about undef value with keep_html option
+    {    # HTML::TableExtract warns about undef value with keep_html option
         $SIG{__WARN__} = sub {
             warn @_ unless $_[0] =~ /uninitialized value in subroutine entry/;
         };
         $te->parse($content);
     }
 
+    my @accounts;
     for my $ts ( $te->tables ) {
 
         # print "Table (", join( ',', $ts->coords ), "):\n";
@@ -97,158 +52,29 @@ sub check_balance {
         for my $row ( $ts->rows ) {
             next if $row->[1] =~ /Totals/;    # Skip total rows
 
-            # Remove any superscripts
-            $row->[0] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-            $row->[1] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-            $row->[2] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-            $row->[3] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
+            ## strip_superscript( @$row[0..3] );
+            strip_html( @$row[ 0 .. 3 ] );
+            trim_whitespace(@$row);
+            remove_currency_symbol( @$row[ 2 .. 3 ] );
+            $row->[0] =~ s{^([\d.-]+).*$}{$1}s;    # Strip all but num from name
 
-            # Simple regex to strip html from cells
-            $row->[0] =~ s{<[^>]*>}{}mg;
-            $row->[1] =~ s{<[^>]*>}{}mg;
-            $row->[2] =~ s{<[^>]*>}{}mg;
-            $row->[3] =~ s{<[^>]*>}{}mg;
-
-            $_ =~ s{^\s*|\s*$}{}g for @$row;    # Trim whitespace
-            $row->[0] =~ s{^([\d.-]+).*$}{$1}s; # Strip all but num from name
-            $row->[2] =~ s/[\$,]//xg;           # Remove $ and , from value
-            $row->[3] =~ s/[\$,]//xg;           # Remove $ and , from value
-
-            # If this is an account with positions, let's go grab that data, too.
-            if ( $row->[0] =~ /\d{4}-\d{4}/ ) {
-                my @positions;
-                my $acct = $row->[0];
-                $acct =~ s/-//;
-
-                if ( $opts{content} ) {
-
-                    # Read in the available files
-                    open my $fh, "<", $acct or confess;
-                    $content = do { local $/ = undef; <$fh> };
-                    close $fh;
-                } else {
-
-                    # Grab the data from the Schwab site
-                    $ua->get(
-                        "https://client.schwab.com/Accounts/Positions/AccountPositionsSummary.aspx?selAcct=$acct"
-                    ) or croak "couldn't load position page for $acct";
-                    $content = $ua->content;
-
-                    if ( $opts{log} ) {
-
-                        # Dump to a log file (based on acct #)
-                        open( my $fh, ">", $acct ) or confess;
-                        print $fh $content;
-                        close $fh;
-                    }
-                }
-
-                my $te = HTML::TableExtract->new(
-                    headers   => [ 'Symbol', 'Quantity', 'Price', 'Change' ],
-                    keep_html => 1,
-                    ## decode    => 0,
-                );
-
-                {
-
-                    # HTML::TableExtract warns about undef value with keep_html option
-                    $SIG{__WARN__} = sub {
-                        warn @_
-                          unless $_[0] =~
-                          /uninitialized value in subroutine entry/;
-                    };
-                    $te->parse($content);
-                }
-
-                for my $ts ( $te->tables ) {
-
-                    # print "Table (", join( ',', $ts->coords ), "):\n";
-                    no warnings 'uninitialized';
-
-                    for my $row ( $ts->rows ) {
-                        next
-                          if $row->[2] eq ''
-                          ; # Skip empty rows (There's an oddity here where it's most efficient to check the Price column)
-                        next if $row->[0] =~ /Total/;    # Skip total rows
-
-                        # Remove any superscripts
-                        $row->[0] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-                        $row->[1] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-                        $row->[2] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-                        $row->[3] =~ s{<sup[^>]*>[^<]*</sup>}{}mg;
-
-                        # Let's call out if these are stocks/bonds/cash/unknown in case the user finds this helpful
-                        if ( $row->[0] =~ m/SymbolRouting/ ) {
-
-                            # This is a Stock
-                            $row->[4] = 'Stock';
-                        } elsif ( $row->[0] =~ m/TradeBondSuperPopUp/ ) {
-
-                            # This is a Bond.  James Bond.
-                            $row->[4] = 'Bond';
-                        } elsif ( $row->[0] =~ m/Cash/ ) {
-
-                            # This is Cash
-                            $row->[4] = 'Cash';
-                        } else {
-
-                            # I don't know what this is
-                            $row->[4] = 'Unknown';
-                        }
-
-                        # Simple regex to strip html from cells
-                        $row->[0] =~ s{<[^>]*>}{}mg;
-                        $row->[1] =~ s{<[^>]*>}{}mg;
-                        $row->[2] =~ s{<[^>]*>}{}mg;
-                        $row->[3] =~ s{<[^>]*>}{}mg;
-
-                        $_ =~ s{^\s*|\s*$}{}g for @$row;    # Trim whitespace
-                        $row->[1] =~ s/[,]//xg;      # Remove , from value
-                        $row->[2] =~ s/[\$,]//xg;    # Remove $ and , from value
-                        $row->[3] =~ s/[\$,]//xg;    # Remove $ and , from value
-
-                        if ( $row->[0] =~ m/Cash/ ) {
-
-                            # The "Cash & Cash Investments" line is screwy, where the value is in the "Change" column.  Let's correct it and set "shares" to be 1, but price to be value.
-                            $row->[0] =
-                              'Cash';   # Trim off the "& Cash Investments" part
-                            $row->[1] = 1;
-                            $row->[2] = $row->[3];
-                        }
-
-                        if ( $row->[4] =~ m/Bond/ ) {
-
-                            # The Bond types use funny math, where bond prices are shown per 100 shares.  Correction is to divide price or quantity by 100.  I elect price.
-                            $row->[2] = $row->[2] / 100;
-                        }
-
-                        push @positions,
-                          {
-                            symbol   => $row->[0],
-                            quantity => $row->[1],
-                            price    => $row->[2],
-                            type     => $row->[4] };
-                    }
-                }
-                $row->[4] = \@positions;
-
-            } else {
-
-                # Probably a banking account, ignore for now
-                $row->[4] = '';
-            }
+            # If this is an account with positions, go grab that data.
+            # If not it is probably a bank account, ignore for now.
+            my @positions =
+              ( $row->[0] =~ /\d{4}-\d{4}/ )
+              ? get_positions( $row->[0], %opts )
+              : ();
 
             push @accounts, (
                 bless {
-                    positions => $row->[4]
-                    ,    # Reference to an array of references to hashes...
                     cash       => $row->[3],
                     balance    => $row->[2],
                     name       => $row->[1],
                     sort_code  => $row->[1],
                     account_no => $row->[0],
+                    statement  => undef,
+                    positions  => \@positions,
                     ## parent       => $self,
-                    statement => undef,
                 },
                 "Finance::Bank::Schwab::Account"
             );
@@ -260,7 +86,194 @@ sub check_balance {
     return @accounts;
 }
 
+sub retrieve_summary_page {
+    my (%opts) = @_;
+
+    # Use the stored page content if requested
+    return slurp( $opts{content} ) if $opts{content};
+
+    croak "Must provide a password" unless exists $opts{password};
+    croak "Must provide a username" unless exists $opts{username};
+
+    # Get the login page
+    $ua->get('https://client.schwab.com/Login/SignOn/CustomerCenterLogin.aspx')
+      or croak "couldn't load inital page";
+
+    # Find the login form, change the action url, then set the username/
+    # password and submit
+    my $login_form = $ua->form_name('aspnetForm')
+      or croak "Couldn't find the login form";
+    $login_form->action('https://client.schwab.com/Login/SignOn/signon.ashx')
+      or croak "Couldn't update the action url on login form";
+    my $username_field =
+      'ctl00$WebPartManager1$CenterLogin$LoginUserControlId$txtLoginID';
+    $login_form->value( $username_field => $opts{username} );
+    $login_form->value( 'txtPassword'   => $opts{password} );
+    $ua->submit() or croak "couldn't sign on to account";
+
+    my $content = $ua->content;
+
+    # Dump to the filename passed in log
+    spew( $opts{log}, $content ) if $opts{log};
+
+    return $content;
+}
+
+sub get_positions {
+    my ( $acct, %opts ) = @_;
+
+    # Only retrieve positions if requested
+    return () unless $opts{get_positions};
+
+    $acct =~ s/-//;
+
+    my $content = retrieve_account_page( $acct, %opts );
+    my @positions = retrieve_account_positions($content);
+}
+
+sub retrieve_account_page {
+    my ( $acct, %opts ) = @_;
+
+    # Used the saved data if content was supplied
+    return slurp("$opts{content}.$acct") if $opts{content};
+
+    # Grab the data from the Schwab site
+    $ua->get(
+        "https://client.schwab.com/Accounts/Positions/AccountPositionsSummary.aspx?selAcct=$acct"
+    ) or croak "couldn't load position page for $acct";
+    my $content = $ua->content;
+
+    # Dump the page to a log file if the log filename was provided
+    spew( "$opts{log}.$acct", $content ) if $opts{log};
+}
+
+sub retrieve_account_positions {
+    my ($content) = @_;
+
+    my $te = HTML::TableExtract->new(
+        headers   => [ 'Symbol', 'Quantity', 'Price', 'Change' ],
+        keep_html => 1,
+        ## decode    => 0,
+    );
+
+    {    # HTML::TableExtract warns about undef value with keep_html option
+        $SIG{__WARN__} = sub {
+            warn @_
+              unless $_[0] =~ /uninitialized value in subroutine entry/;
+        };
+        $te->parse($content);
+    }
+
+    my @positions;
+    for my $ts ( $te->tables ) {
+
+        # print "Table (", join( ',', $ts->coords ), "):\n";
+        no warnings 'uninitialized';
+
+        for my $row ( $ts->rows ) {
+
+            next if $row->[2] eq '';         # Skip empty rows
+            next if $row->[0] =~ /Total/;    # Skip total rows
+
+            strip_superscript( @$row[ 0 .. 3 ] );
+            strip_html( @$row[ 0 .. 3 ] );
+            trim_whitespace(@$row);
+            remove_commas( $row->[1] );
+            remove_currency_symbol( @$row[ 2, 3 ] );
+
+            # Note if these are stocks/bonds/cash/unknown
+            my $type;
+          SWITCH: {
+                local $_ = $row->[0];
+                m/SymbolRouting/       and $type = 'Stock', last;
+                m/TradeBondSuperPopUp/ and $type = 'Bond',  last;
+                m/Cash/                and $type = 'Cash',  last;
+                $type = 'Unknown';
+            }
+
+            # The "Cash & Cash Investments" line is screwy, where the value is
+            # in the "Change" column. Let's correct it and set "price" to be 1,
+            # and "shares" be value.
+            if ( $row->[0] =~ m/Cash/ ) {
+                $row->[0] = 'Cash';      # Trim "& Cash Investments"
+                $row->[1] = $row->[3];
+                $row->[2] = 1;
+            }
+
+            # The Bond types use funny math, where bond prices are shown per
+            # 100 shares.  Correction is to divide price or quantity by 100.  I
+            # elect price.
+            if ( $type =~ m/Bond/ ) {
+                $row->[2] = $row->[2] / 100;
+            }
+
+            push @positions,
+              bless {
+                symbol   => $row->[0],
+                quantity => $row->[1],
+                price    => $row->[2],
+                type     => $type,
+              },
+              'Finance::Bank::Schwab::Account::Positions';
+        }
+    }
+    return @positions;
+
+}
+
+sub strip_html {
+
+    # Simple regex to strip html from cells. Not the best practice, but this is
+    # certainly not the most fragile part of this module.
+    s{<[^>]*>}{}mg for grep { defined } @_;
+}
+
+sub trim_whitespace {
+    s{^\s*|\s*$}{}g for grep { defined } @_;
+}
+
+sub remove_commas {
+    s/[,]//xg for grep { defined } @_;
+}
+
+sub remove_currency_symbol {
+    s/[\$,]//xg for grep { defined } @_;
+}
+
+sub strip_superscript {
+    s{<sup[^>]*>[^<]*</sup>}{}mg for grep { defined } @_;
+}
+
+sub spew {
+    my ( $filename, $content ) = @_;
+
+    open( my $fh, ">", $filename ) or confess;
+    print $fh $content;
+    close $fh;
+}
+
+sub slurp {
+    my ($filename) = @_;
+
+    open my $fh, "<", $filename or confess;
+    my $content = do { local $/; <$fh> };
+    close $fh;
+
+    return $content;
+}
+
 package Finance::Bank::Schwab::Account;
+
+# Basic OO smoke-and-mirrors Thingy
+no strict;
+
+sub AUTOLOAD {
+    my $self = shift;
+    $AUTOLOAD =~ s/.*:://x;
+    return $self->{$AUTOLOAD};
+}
+
+package Finance::Bank::Schwab::Account::Positions;
 
 # Basic OO smoke-and-mirrors Thingy
 no strict;
@@ -283,23 +296,26 @@ Finance::Bank::Schwab - Check your Charles Schwab accounts from Perl
 
   use Finance::Bank::Schwab;
   my @accounts = Finance::Bank::Schwab->check_balance(
-      username => "xxxxxxxxxxxx",
-      password => "12345",
+      username     => "xxxxxxxxxxxx",
+      password     => "12345",
+      get_position => 1,
   );
-  
-  foreach (@accounts) {
+
+  for ( @accounts ) {
       printf "%20s : %8s / %8s : USD %9.2f USD %9.2f\n",
-      $_->name, $_->sort_code, $_->account_no, $_->cash, $_->balance;
-      if($_->positions){ 
-          foreach my $arrayref ($_->positions) {
-              foreach $hashref (@$arrayref){
-                  print "\t" . $$hashref{"type"} . "\t" . $$hashref{"symbol"} . "\t" . $$hashref{"quantity"} . " Shares \@\t\$" . $$hashref{"price"} ."\n";
-              }
-              print "\n";
-          } 
+          $_->name, $_->sort_code, $_->account_no, $_->cash, $_->balance;
+
+      for my $position ( @{ $_->positions } ) {
+          printf "# \t%-10s %-10s %10s Shares \@ \$%-15s\n",
+            $position->type,
+            $position->symbol,
+            $position->quantity,
+            $position->price;
       }
+      print "\n";
+
   }
-  
+
 =head1 DESCRIPTION
 
 This module provides a rudimentary interface to the Charles Schwab site.
--- t/10-check_balance.t
+++ t/10-check_balance.t
@@ -7,24 +7,36 @@ use Finance::Bank::Schwab;
 my $userid = $ENV{F_C_SCHWAB_USERID};
 my $passwd = $ENV{F_C_SCHWAB_PASSWD};
 
-plan skip_all => "- Need password to fully test. To enable tests set F_C_SCHWAB_USERID F_C_SCHWAB_PASSWD environment variables."
-		unless $userid && $passwd;
-plan tests => 3;
+plan skip_all => "- Need password to fully test. To enable tests set "
+  . "F_C_SCHWAB_USERID F_C_SCHWAB_PASSWD environment variables."
+  unless $userid && $passwd;
+plan tests => 4;
 
 # Test set 2 -- create client with ordered list of arguements
 my @accounts = Finance::Bank::Schwab->check_balance(
-    			'username'	=> $userid,
-    			'password'	=> $passwd,
-                # 'log' => 'tmp.log',
-                # 'content' => 'tmp.log',
-		 );
+    username => $userid,
+    password => $passwd,
+    ## log => 'log/tmp.log',
+    content       => 'log/tmp.log',
+    get_positions => 1,
+);
 
 ok @accounts, "check_balance returned a non-empty array";
-isa_ok $accounts[0], 'Finance::Bank::Schwab::Account', "check_balance returned a new Finance::Bank::Schwab::Account object";
-ok $accounts[0]->account_no, 'Returned a non-false value for the account number';
+isa_ok $accounts[0], 'Finance::Bank::Schwab::Account', "check_balance()";
+ok $accounts[0]->account_no, 'Returned a true value for the account number';
+ok $accounts[0]->positions,  'Returned a non-empty list of positions';
 
-for (@accounts){
-	printf "# %18s : %8s / %8s : \$ %9.2f\n",
-	    $_->name, $_->sort_code, $_->account_no, $_->balance;
+for (@accounts) {
+    printf "# %18s : %8s / %8s : \$ %9.2f\n",
+      $_->name, $_->sort_code, $_->account_no, $_->balance;
+
+    for my $position ( @{ $_->positions } ) {
+        printf "# \t%-10s %-10s %10s Shares \@ \$%-15s\n",
+          $position->type,
+          $position->symbol,
+          $position->quantity,
+          $position->price;
+    }
+    print "# \n";
 }
 
